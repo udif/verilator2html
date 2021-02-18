a59e9d9d222f4b5f5124d642d0be2ff2cea7fb3f
Udi Finkelstein <github@udifink.com>
2021-02-18 12:42:28 +0200

    Initial version

---
+++ README.md
@@ -0,0 +1,9 @@
+verilator2html
+==============
+This is my hack solution for converting Verilator logs into browseable HTML reports. It allows you to browse all your errors and warnings, and sort/filter them any way you like.  
+The core Javascript code used is taken from https://github.com/chestercharles/excel-bootstrap-table-filter
+As my knowledge on frontend web development is nonexistent and mostly based on copy/paste and stackoverflow, feel free to improve.
+
+The dist/ files in my repo are taken from the following version:  
+https://raw.githubusercontent.com/chestercharles/excel-bootstrap-table-filter/4a88f70bb6857131d6b048cd4ff78da327064532/dist/excel-bootstrap-table-filter-bundle.min.js  
+https://raw.githubusercontent.com/chestercharles/excel-bootstrap-table-filter/4a88f70bb6857131d6b048cd4ff78da327064532/dist/excel-bootstrap-table-filter-style.css  
---
+++ verilator2html.py
@@ -0,0 +1,78 @@
+#!/bin/env python3
+import sys
+import os
+
+if len(sys.argv) != 3:
+    print("Usage: {} <infile> <outfile>".format(sys.argv[0]))
+    sys.exit()
+
+html_h = """
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Excel Bootstrap Table Filter</title>
+  <link href="https://www.jqueryscript.net/css/jquerysctipttop.css" rel="stylesheet" type="text/css">
+    <!-- Load jQuery -->
+  <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+  <!-- Load the plugin bundle. -->
+  <script src="https://raw.githubusercontent.com/chestercharles/excel-bootstrap-table-filter/master/dist/excel-bootstrap-table-filter-bundle.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="https://raw.githubusercontent.com/chestercharles/excel-bootstrap-table-filter/master/dist/excel-bootstrap-table-filter-style.css" />
+  <style>
+  body { background-color:#fafafa;}
+  .container { margin:150px auto;}
+  </style>
+</head>
+
+<body>
+<div class="jquery-script-clear"></div>
+</div>
+</div>
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <table id="table" class="table table-bordered table-intel">
+        <thead>
+          <tr class="table-info">
+            <th>Type</th>
+            <th>File</th>
+            <th>Line</th>
+            <th>Col</th>
+            <th>Message</th>
+          </tr>
+        </thead>
+        <tbody>
+"""
+html_t = """
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
+
+<script>
+  // Use the plugin once the DOM has been loaded.
+  $(function () {
+    // Apply the plugin 
+    $('#table').excelTableFilter();
+  });
+</script>
+</body>
+</html>
+"""
+
+with open(sys.argv[1], "r") as fh:
+    with open(sys.argv[2], "w") as fl:
+        print(html_h, file=fl)
+        for l in fh.readlines():
+            if not l.startswith('%'):
+                continue
+            lw = ''
+            for w in l.split(':', 4):
+                lw += '<td>{}</td>'.format(w)
+            print('<tr>{}</tr>'.format(lw), file=fl)
+        print(html_t, file=fl)
+
